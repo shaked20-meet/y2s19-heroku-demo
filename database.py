e1de891240c566cf055aa92384f1c78072d2c3ee
myname21-meet <myname21@meet.mit.edu>
2019-08-01 12:09:49 -0400

    labs

--- database.py
+++
@@ -1,8 +0,0 @@
-def add_user(name, secret_word):
-    user_object = User(username=name,  password=secret_word)
-    session.add(user_object)
-    session.commit()
-
-
-def get_user(name):
-    return session.query(User).filter(username=name).first()
