6839fabd9c9466597ee58fda58c2a2ca801c678f
myname21-meet <myname21@meet.mit.edu>
2019-08-01 15:35:45 -0400

    lab

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('shady.html')
-@app.route('/login', methods=['POST'])
-def login():
-    user = get_user(request.form['username'])
-    if user.password == request.form['password']:
-        login_session['name'] = user.username
+
 
 
 if __name__ == '__main__':
